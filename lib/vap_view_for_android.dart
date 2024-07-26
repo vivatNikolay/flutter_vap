8d532b2e6aa56571cf8094957aca24be09367580
gasaichandesu <ourea121@gmail.com>
2024-07-26 11:28:13 +0400

    feat: :sparkles: add loops argument to control how many times animation should be played

--- lib/vap_view_for_android.dart
+++ lib/vap_view_for_android.dart
@@ -2,9 +2,18 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class VapViewForAndroid extends StatelessWidget {
+  const VapViewForAndroid({
+    this.loops = 1,
+  });
+
+  /// Number of times video should be played
+  final int loops;
+
   @override
   Widget build(BuildContext context) {
-    final Map<String, dynamic> creationParams = <String, dynamic>{};
+    final Map<String, dynamic> creationParams = <String, dynamic>{
+      'loops': loops,
+    };
     return AndroidView(
       viewType: "flutter_vap",
       layoutDirection: TextDirection.ltr,
